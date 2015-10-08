69e088ec83dbb56ff10616b142a8a539eda0768c
Varun Bansal <varun13168@iiitd.ac.in>
2015-10-08 23:33:52 +0530

    Name error exception handled!

---
+++ tohWithExceptionHandling.py
@@ -0,0 +1,21 @@
+def Towers(height, start, target, help,R):
+    if height>=1:
+        Towers(height-1,start,help,target,R)
+        print "Move disk %s from %s to %s" %(R[height-1]-1,start, target)
+        Towers(height-1,help,target,start,R)
+
+
+
+x = input("Enter number of disks")
+
+R=range(x)
+for i in range(x):
+    R[i]+=1
+
+Rev=R[::-1]
+
+A='A'
+B='B'
+C='C'
+
+Towers(x,A,B,C,Rev)
